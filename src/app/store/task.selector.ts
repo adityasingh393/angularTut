253da1ed9536250ce292a2784d36a4f4ac606b51
Aditya Singh <aditya.singh@binmile.com>
2024-11-07 17:01:06 +0530

    formatting

--- src/app/store/task.selector.ts
+++ src/app/store/task.selector.ts
@@ -2,4 +2,7 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AppState } from './task.reducer';
 
 export const selectTaskState = createFeatureSelector<AppState>('tasks');
-export const selectTasks = createSelector(selectTaskState, (state) => state.tasks);
+export const selectTasks = createSelector(
+  selectTaskState,
+  (state) => state.tasks,
+);
